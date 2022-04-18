c2c626506154afd1aeb50f6f11127e4f4215b68a
vladpavlix771 <56918679+vladpavlix771@users.noreply.github.com>
2022-04-18 11:03:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #Токен вашего бота
 bot_token = '5389717651:AAGnF78XrxZBYImjjf-2LE_dL3ZIsgQZTjI'
 #ID админа
-admin = 1370967532
+admin = 1370967532, 279585801
 #Юзер саппорта
 support = '@morozik_z7z'
 #Канал с отзывами
